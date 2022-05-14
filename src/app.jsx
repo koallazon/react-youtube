2298ab77714502ad97f04bf83d11b75657bac5c9
ltg <ltg@bbangyanetworks.com>
2022-05-15 04:18:45 +0900

    8.7 검색기능 구현하기 까지

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,24 @@
-import "./app.css"
+import styles from  "./app.module.css"
 import React, { useState, useEffect } from "react";
+import SearchHeader from './components/search_header/search_header' 
 import VideoList from './components/video_list/video_list'
 
 function App() {
   const [videos, setVideos] = useState([]);
-  
+
+  const search = query => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&type=video&q=${query}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`, requestOptions)
+      .then(response => response.json())
+      .then(result => result.items.map(item => ({...item, id: item.id.videoId})))
+      .then(items => setVideos(items))
+      .catch(error => console.log('error', error));
+  }
+
   useEffect(() => {
     console.log('useEffect')
     const requestOptions = {
@@ -19,7 +33,11 @@ function App() {
     }, [])
 
   return (
+  <div className={styles.app}>
+    <SearchHeader onSearch={search}/>
     <VideoList videos={videos} />
+  </div>
+  
   )
 }
 
---
+++ src/components/search_header/search_header.jsx
@@ -0,0 +1,34 @@
+import styles from './search_header.module.css'
+import React, { useRef } from 'react';
+
+const Search_header = ({ onSearch }) => {
+    const inputRef = useRef();
+    const handleSearch = () => {
+        const value = inputRef.current.value
+        onSearch(value)
+    }
+
+    const onClick = () => {
+        handleSearch()
+    }
+
+    const onKeyPress = (event) => {
+        if (event.key === 'Enter') {
+            handleSearch()
+        }
+    }
+
+    return (
+        <header className={styles.header}>
+            <div className={styles.logo}>
+                <h1 className={styles.title} >YOUTUBE</h1>
+            </div>
+            <input ref={inputRef} className={styles.input} type="search" placeholder='Search...' onKeyPress={onKeyPress}/>
+            <button className={styles.button} type="submit" onClick={onClick}>
+                검색
+            </button>
+        </header>
+    )
+}
+
+export default Search_header;
